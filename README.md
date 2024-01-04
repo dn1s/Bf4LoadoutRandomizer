c6156cd2d166fe6b91596d0ff28ef463a0872a79
d_n1s <dominik.n.schuster@gmail.com>
2024-01-05 00:39:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## **QUICK START:**
+
+This is a loadout randomizer for battlefield 4 written in python
+
+This script can be run as flask site or just from __init__.py directly
+
+Example output from standalone:
+
+```
+Class: Support, Weapon: RPK-12 - Attachments: ['M145 (3.4X)', 'Vertical Grip', 'Magnifier (2X)', 'Flash Hider'], Handgun: SW40 - Attachments: ['Delta (RDS)', 'Laser Sight', 'Compensator'], Granade: M34 Incendiary, Gadget: Ballistic Shield
+```
+
+Run as standalone:
+```
+cd Bf4LoadoutRandomizer
+python3 ./__init__.py
+```
+
+Python Flask module has to be installed to run as website:
+```
+pip install Flask
+```
+
+Run as website:
+```
+cd Bf4LoadoutRandomizer
+flask --app loadout_webserver run
+```
---
+++ __init__.py
@@ -0,0 +1,59 @@
+import csv
+from random import choice
+
+class Loadouts:
+    def __init__(self):
+        self.gear = {}
+        # This can be set to absolute path to bf4_attachments.csv file for convenience
+        self.filepath = '.'
+        self.filename = 'bf4_attachments.csv'
+        self.bf_class = ''
+        self.bf_classes = ['Assault', 'Engineer', 'Support', 'Recon']
+
+    def get_data_from_csv(self) -> list[dict]:
+        with open(f'{self.filepath}/{self.filename}', 'r') as attachments_file:
+            reader = csv.DictReader(attachments_file)
+            self.gear = [row for row in reader]
+
+    def choose_class(self) -> str:
+        self.bf_class = choice(self.bf_classes)
+        return self.bf_class
+
+    def choose_loadout(self, equipment_filter:str) -> str:
+        equipments = []
+        for gear in self.gear:
+            if gear['Class'] == equipment_filter or (equipment_filter in self.bf_classes and gear['Class'] == 'All'):
+                if gear['Weapon'] not in equipments:
+                    equipments.append(gear['Weapon'])
+        return choice(equipments)
+
+    def choose_weapon_attachments(self, weapon:str, unlock_filter:str=None) -> list:
+        attachment_types = []
+        attachments = []
+        selected_attachments = []
+
+        for gear in self.gear:
+            if weapon in gear.values():
+                attachments.append([gear['Attachment'], gear['Attachment Type'], gear['Unlock']])
+                if gear['Attachment Type'] not in attachment_types:
+                    attachment_types.append(gear['Attachment Type'])
+
+        for attachment_type in attachment_types:
+            selected_attachments.append(choice([attachment for attachment in attachments if attachment_type in attachment and not unlock_filter in attachment])[0])
+
+        return selected_attachments
+
+
+if __name__ == '__main__':
+    loadout = Loadouts()
+    loadout.get_data_from_csv()
+    bf_class = loadout.choose_class()
+    primary_weapon = loadout.choose_loadout(equipment_filter=bf_class)
+    primary_weapon_attachments = loadout.choose_weapon_attachments(primary_weapon, unlock_filter=None)
+    secondary_weapon = loadout.choose_loadout(equipment_filter='Handgun')
+    secondary_weapon_attachments = loadout.choose_weapon_attachments(secondary_weapon, unlock_filter=None)
+    granade = loadout.choose_loadout(equipment_filter='Granade')
+    gadget = loadout.choose_loadout(equipment_filter=f'{bf_class}_gadgets')
+
+    print(f'Class: {bf_class}, Weapon: {primary_weapon} - Attachments: {primary_weapon_attachments}, Handgun: {secondary_weapon} - \
+Attachments: {secondary_weapon_attachments}, Granade: {granade}, Gadget: {gadget}')
